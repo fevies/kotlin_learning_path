119e28fa621f41e0d8db9db199b04edf7212eef4
fevies <elviesodhiambo@gmail.com>
2025-05-19 10:06:45 +0300

    exercise submitted

--- src/controlFlow/miiniExercise.kt
+++ src/controlFlow/miiniExercise.kt
@@ -34,4 +34,35 @@ fun main() {
     //determine if reader comes before author
     val readerBeforeAuthor = reader < author
     println("does reader come before author $readerBeforeAuthor")
+    //Write a when expression that takes an age as an integer and prints out the life
+    //stage related to that age. You can make up the life stages, or use the
+    //categorization as follows: 0-2 years, Infant; 3-12 years, Child; 13-19 years,
+    //Teenager; 20-39, Adult; 40-60, Middle aged; 61+, Elderly
+
+    val age = 13
+    val ageCheck: String = when(age){
+        in 0..2 ->"infant"
+        in 3 ..12 -> "child"
+        in 13 ..19 -> "teenager"
+        in  20 .. 39 -> "adult"
+        in 40 .. 60 -> "middle age"
+        else -> "elderly"
+
+    }
+    println(ageCheck)
+    //Write a when expression that destructures a Pair containing a string and an
+    //integer. The string is a name, and the integer is an age. Use the same cases that
+    //you used in the previous exercise to print out the name followed by the life stage.
+    //For example, for myself it would print out "Joe is an adult.".
+
+    val (name: String, ageTwo: Int) = Pair("Joe",18)
+    val ageCheckTwo = when (ageTwo) {
+        in 0 .. 2 -> "infant"
+        in 3 ..12 -> "child"
+        in 13 ..19 -> "teenager"
+        in 20 .. 39 -> "adult"
+        in 40 .. 60 -> "middle age"
+        else -> "elderly"
+    }
+    println("$name is a $ageCheckTwo")
 }
